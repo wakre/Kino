208504f3aeff75341790a427891b01b56f340801
wakre <143410786+wakre@users.noreply.github.com>
2024-04-24 20:30:19 +0200

    Update KinoRepository.java

--- Kino/src/main/java/com/example/kino/KinoRepository.java
+++ Kino/src/main/java/com/example/kino/KinoRepository.java
@@ -10,23 +10,23 @@ import java.util.List;
 @Repository
 public class KinoRepository {
     @Autowired
-    private JdbcTemplate rep;
+    private JdbcTemplate db;
 
     public void lagrebillett (Billetter billetter) {
         String sql = "INSERT INTO Billetter(velgFilm, antall, fornavn, etternavn, telefonnr, epost) VALUES(?,?,?,?,?,?)";
-        rep.update(sql, billetter.getVelgFilm(), billetter.getAntall(), billetter.getFornavn(), billetter.getEtternavn(), billetter.getTelefonnr(), billetter.getEpost());
+        db.update(sql, billetter.getVelgFilm(), billetter.getAntall(), billetter.getFornavn(), billetter.getEtternavn(), billetter.getTelefonnr(), billetter.getEpost());
     }
 
     public List<Billetter> hentAlle(){
         String sql = "SELECT * FROM Billetter";
-        List<Billetter> alleBilletter = rep.query(sql, new BeanPropertyRowMapper(Billetter.class));
+        List<Billetter> alleBilletter = db.query(sql, new BeanPropertyRowMapper(Billetter.class));
         alleBilletter.sort(Comparator.comparing(Billetter::getEtternavn));
         return alleBilletter;
     }
 
     public void slettAlle(){
         String sql ="DELETE FROM Billetter";
-        rep.update(sql);
+        db.update(sql);
     }
 }
 
