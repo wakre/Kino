6deac9588addb0868870855d23af9e607fe606b9
wakre <143410786+wakre@users.noreply.github.com>
2024-04-25 23:02:03 +0200

    Update skjema.sql

--- Kino/src/main/resources/skjema.sql
+++ Kino/src/main/resources/skjema.sql
@@ -1,10 +1,11 @@
 
 CREATE TABLE Billetter(
     id INTEGER AUTO_INCREMENT NOT NULL,
-    film VARCHAR(223) NOT NULL,
+    VelgFilm VARCHAR(223) NOT NULL,
     antall INTEGER NOT NULL,
     fornavn VARCHAR(223) NOT NULL,
     etternavn VARCHAR(223) NOT NULL,
     telefonnr INTEGER NOT NULL,
+    epost VARCHAR(233) NOT NULL,
     PRIMARY KEY (id)
 );
